d46b8fa98318126d6444869292234e6c9bbfe260
Yannik Warnecke <yannik.warnecke@uni-muenster.de>
2025-03-06 09:28:05 +0100

    Removed data integration chapter for now

--- content/api_integration/_meta.js
+++ content/api_integration/_meta.js
@@ -1,6 +1,5 @@
 export default {
     api_overview: 'API Overview',
     api_usage: 'Using the API',
-    integration: 'Integrating with Other Systems',
     extending_cinnamon: 'How to extend the Platform'
 }
